2780df02c3336664e8c9a10ab767225fe3faaf0a
Shanzhuo <shanzhuo.zhang@gmail.com>
2018-01-29 11:58:42 +0000

    Save figure in plotvc and resize all elements to produce proper sized figure.

--- util_bundle/plotvc.py
+++ util_bundle/plotvc.py
@@ -1,24 +1,27 @@
 """
 Voltage_Clamp:
- DT: 0.02
+  DT: 0.1
 
- Target: I_CaT
- Protocol:
-   HOLDING_V: -90
-   HOLDING_TIME: 200
+  Target: I_to
+  Protocol:
+    HOLDING_V: -75
+    HOLDING_TIME: 200
 
-   # pre-pulse means a period between holding and test
-   PREPULSE_V: -60
-   PREPULSE_TIME: 300
+    # pre-pulse means a period between holding and test
+    PREPULSE_V: -60
+    PREPULSE_TIME: 150
 
-   # test potential
-   VC_START: -40
-   VC_END: 11.0
-   VC_STEP: 10.0
-   DURATION: 200 # test_time
+    # test potential
+    VC_START: -105.0
+    VC_END: 35.01
+    VC_STEP: 10.0
+    DURATION: 200 # test_time
 
-   TAIL_V: -90
-   TAIL_TIME: 200
+    POSTPULSE_V: 35
+    POSTPULSE_TIME: 200
+
+    TAIL_V: -75
+    TAIL_TIME: 150
 """
 import numpy as np
 import matplotlib.pyplot as plt
@@ -39,19 +42,22 @@ def plotvc(vc_yaml, start=1, end=0):
     mpl_setting.set_matplotlib_default()
     config = yaml.load(vc_yaml)
 
+    plt.figure(figsize=[mpl_setting.fig_size[0]/4, mpl_setting.fig_size[1]/8])
+
     # must be wrapped by 'Voltage_Clamp' and 'Protocol'
     config = config['Voltage_Clamp']['Protocol']
 
     holding = [config['HOLDING_V']] * config['HOLDING_TIME']
     prepulse = [config['PREPULSE_V']] * config['PREPULSE_TIME']
+    postpulse = [config['POSTPULSE_V']] * config['POSTPULSE_TIME']
     tail = [config['TAIL_V']] * config['TAIL_TIME']
 
-    time = range(config['HOLDING_TIME'] + config['PREPULSE_TIME'] + config['DURATION'] + config['TAIL_TIME'])
+    time = range(config['HOLDING_TIME'] + config['PREPULSE_TIME'] + config['DURATION'] + config['POSTPULSE_TIME'] + config['TAIL_TIME'])
 
     test_v = np.arange(config['VC_START'], config['VC_END'], config['VC_STEP'])
     for v in test_v:
         test = [v] * config['DURATION']
-        voltage = holding + prepulse + test + tail
+        voltage = holding + prepulse + test + postpulse + tail
         plt.plot(time, voltage, 'k')
 
     ax = plt.gca()
@@ -69,15 +75,36 @@ def plotvc(vc_yaml, start=1, end=0):
             s='{:.0f} ms'.format(config['DURATION']),
             va='bottom', ha='center')
 
+    # text on prepulse and postpulse
+    if config['PREPULSE_TIME'] >= 200:
+        ax.text(x=config['HOLDING_TIME'] - 10,
+                y=config['PREPULSE_V'],
+                s='{:.0f} mV'.format(config['PREPULSE_V']),
+                va='center', ha='right')
+        ax.text(x=config['HOLDING_TIME'] + 0.5 * config['PREPULSE_TIME'],
+                y=config['PREPULSE_V'] + 1,
+                s='{:.0f} ms'.format(config['PREPULSE_TIME']),
+                va='bottom', ha='center')
+    if config['POSTPULSE_TIME'] >= 200:
+        ax.text(x=config['HOLDING_TIME'] + config['PREPULSE_TIME'] + config['DURATION'] + config['POSTPULSE_TIME'] + 10,
+                y=config['POSTPULSE_V'],
+                s='{:.0f} mV'.format(config['POSTPULSE_V']),
+                va='center', ha='left')
+        ax.text(x=config['HOLDING_TIME'] + config['PREPULSE_TIME'] + config['DURATION'] + 0.5 * config['POSTPULSE_TIME'],
+                y=config['POSTPULSE_V'] + 1,
+                s='{:.0f} ms'.format(config['POSTPULSE_TIME']),
+                va='bottom', ha='center')
+
     # text on holding and tailing
     ax.text(x=start,
             y=config['HOLDING_V'] + 1,
             s='{:.0f} mV'.format(config['HOLDING_V']),
             va='bottom', ha='left')
-    ax.text(x=time[-1] - end,
-            y=config['TAIL_V'] + 1,
-            s='{:.0f} mV'.format(config['TAIL_V']),
-            va='bottom', ha='right')
+    if config['TAIL_TIME'] >= 200:
+        ax.text(x=time[-1] - end,
+                y=config['TAIL_V'] + 1,
+                s='{:.0f} mV'.format(config['TAIL_V']),
+                va='bottom', ha='right')
 
     # reset xlimit
     ax.set_xlim(start, time[-1] - end)
@@ -85,6 +112,7 @@ def plotvc(vc_yaml, start=1, end=0):
     # remove axis
     ax.set_axis_off()
 
+    plt.savefig('voltage.png')
     plt.show()
 
 
